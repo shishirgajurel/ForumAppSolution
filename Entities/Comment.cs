b9ab975295b096988645eb1453af179dcced5370
shishirgajurel <shishirgajurel999@gmail.com>
2025-09-12 21:36:46 +0200

    Created User, Post, and Comment entities

---
+++ Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Body { get; set; }
+    
+    // Foreign key relationship to Post
+    public int PostId { get; set; }
+    // Foreign key relationship to User
+    public int UserId { get; set; }
+}
---
+++ Entities/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    
+    // Foreign key relationship to User
+    public int UserId { get; set; }
+}
